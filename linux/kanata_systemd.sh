a9f747cde5a23e906c49d21c9c5690223ec04aeb
Joshua Lucas <joshdlucas@gmail.com>
2025-06-02 21:06:28 -0500

    Fix systemd setup script.

--- linux/kanata_systemd.sh
+++ linux/kanata_systemd.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
-mkdir -p /etc/kanata
-sudo cp -f ../config/common.kbd /etc/kanata/common.kbd
-sudo cp -f ../config/rk65_Colemak_DHVK.kbd /etc/kanata/kanata.kbd
-sudo 
-sudo install -m 644 kanata.service /lib/systemd/system/kanata.service
-sudo systemctl daemon-reload # maybe this will be required when changing the service file
-sudo systemctl start kanata
-sudo systemctl enable kanata
+sudo cp ../binary/kanata /usr/bin
+
+sudo groupadd uinput
+sudo usermod -aG input $USER
+sudo usermod -aG uinput $USER
+sudo cp 99-input.rules /etc/udev/rules.d/
+
+sudo udevadm control --reload-rules && sudo udevadm trigger
+
+ls -l /dev/uinput
+
+sudo modprobe uinput
+
+mkdir -p ~/.config/systemd/user
+
+cp kanata.service ~/.config/systemd/user/
+
+mkdir ~/.config/kanata
+cp -r ../config ~/.config/kanata
+
+systemctl --user daemon-reload
+systemctl --user enable kanata.service
+systemctl --user start kanata.service
+systemctl --user status kanata.service   # check whether the service is running
---
+++ linux/start_kanata_Precision.sh
@@ -0,0 +1,1 @@
+sudo ~/Repositories/kanata-config/binary/kanata -c ~/Repositories/kanata-config/config/Dell_Precision_Colemak_DHVK.kbd
